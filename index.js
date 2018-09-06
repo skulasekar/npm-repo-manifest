4a70e8f90ab93a0d537302c1ab5b0de299cdbd4d
Santhosh Kulasekar <santhosh.mk@gmail.com>
2018-09-05 22:48:19 -0400

    Added function spec in place for getting new project name

--- index.js
+++ index.js
@@ -41,25 +41,27 @@ function process(err, data) {
       original_git_urls.push(orig_git_url);
 
       // Add the new git url for creation
-      var new_project_name = project_name.split('/').pop()
+      var new_project_name = function() {
+        return project_name.split('/').pop()
+      }
       // console.dir(new_project_name)
       var new_git_url = new_remote + (new_remote.charAt(new_remote.length - 1) == '/' ? new_project_name : "/" + new_project_name) + ".git"
       new_git_urls.push(new_git_url)
 
        console.log(command)
-       // if(index === 0) {
+       if(index === 0) {
          var proc = child_process.spawn(command, [new_project_name, orig_git_url])
          proc.stdout.on("data", function(data) {
-           console.log(data.toString())
+           console.log("INFO: " + data.toString())
          });
          proc.stderr.on("data", function(data) {
-           console.log(data.toString())
+           console.log("ERROR: " + data.toString())
          });
          proc.on("exit", function(data) {
-           console.log("Finished Execution")
+           console.log("Finished Execution of module " + new_project_name)
            // Call back some function here
          });
-       // }
+       }
     })
   });
 
